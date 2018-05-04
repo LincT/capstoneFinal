a62fbd7b2f726a77f6613fdc96d221972e075d13
Linc <test@testserver.com>
2018-05-04 14:25:48 -0500

    added proper __str__ functions to both users and device_manager

--- app/device_manager.py
+++ app/device_manager.py
@@ -10,7 +10,7 @@ from app.sql_handler import DataBaseIO as dbio  # sql handler, really some of my
 class DeviceCache:
 
     DeviceCache = dbio('device_database')
-    table_name = 'users'
+    table_name = 'devices'
 
     def __init__(self):
         """
@@ -26,24 +26,18 @@ class DeviceCache:
                                       'maintenance_downtime text')
 
     def __str__(self):
-        """
-        might refactor this to live in sql_handler
-        :return:
-        """
-        tables = [str(each) for each in self.DBCache.spew_tables()]
-        verbose_table_data = ""
-        for each in tables:
-            fields = ", ".join(item for item in self.DBCache.spew_header(each))
-            contents = "\n".join("\t\t" + str(item) for item in self.DBCache.execute_query(each))
-            verbose_table_data += str("table: " + each + "\n\tfields: " + fields + "\n\tcontents:\n" + contents)
+        fields = ", ".join(item for item in self.DBCache.spew_header(self.table_name))
+        contents = "\n".join("\t\t" + str(item) for item in self.DeviceCache.execute_query(self.table_name))
+        table_data = str("table: " + self.table_name + "\n\tfields: " + fields + "\n\tcontents:\n" + contents)
+        return table_data
 
-        return verbose_table_data
+    def add_device(self):
+        device_name = input("please enter the device name to add:\n")
 
-    def add_user(self, username, password):
         pass
 
-    def remove_user(self, username):
+    def remove_device(self, username):
         pass
 
-    def validate_user(self):
+    def modify_device(self,):
         pass
