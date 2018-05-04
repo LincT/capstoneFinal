97090e6326a2b34cbe4ccb35fedda3134c885404
Linc <test@testserver.com>
2018-05-04 18:40:57 -0500

    added manual check function for demonstration purposes to main
    switched from using os to using subprocess to better
    control output from network commands
    
    preliminary work on recurring service.
    also named tuples
    
    updated table structure for device records

--- app/main.py
+++ app/main.py
@@ -1,37 +1,94 @@
-# business logic should live here.
 import logging  # https://docs.python.org/3/howto/logging-cookbook.html
 import datetime
+import time
+from collections import namedtuple
 
+from app.users import UserManager as users
+from app.device_manager import DeviceCache as devices
+import app.email_manager as alerts
+from app.conntest import ConnectionTester
 
 def show_menu(options_list):
     """
     :type options_list: list
     """
-    # displays a list of choices and then returns user result
+    valid_selection = False
     options_list.append("quit")
-    menu = "Please choose from one of the available options: \n"
-    for option in options_list:
-        menu += "{}:\t{}\n".format(list(options_list).index(option)+1, option)
-    choice = input(menu)
-    return choice
+    while not valid_selection:
+        # displays a list of choices and then returns user result
+
+        menu = "Please choose from one of the available options: \n"
+        for option in options_list:
+            menu += "{}:\t{}\n".format(options_list.index(option) + 1, option)
+        choice = input(menu)
+        if choice.isnumeric():
+            if int(choice) == len(options_list):
+                exit(0)
+            elif int(choice) < len(options_list):
+                return options_list[int(choice)-1]
+
+        elif choice in options_list:
+            if choice == options_list[-1]:
+                exit(0)
+            else:
+                return choice
+        else:
+            continue
 
 
 def timed_execution(increment=1):
-    # get current_second
-    current_second = int(datetime.datetime.utcnow().strftime("%S"))
+    # get current_minute and add 1 to avoid division by 0 error
+    current_minute = int(datetime.datetime.utcnow().strftime("%M"))+1
 
-    if (current_second == 0) or (current_second % increment == 0):
+    # to determine if we are to do the thing, see if the chosen increment
+    # divides perfectly into the current second (1-60)
+    if current_minute % increment == 0:
         # do the thing
         return True
+    else:
+        return False
+
+
+def user_administration():
+    print("user_administration ran")
+
+
+def monitoring():
+    pass
+
+
+def manual_server_check():
+    host = input("host dns or ipv4?\n")
+    port = int(input("port on host to check?\n"))
+    ConnectionTester.conn_check(host=host, port=port)
+
+
+def run_service():
+    device = namedtuple("device",
+                        "device_id "
+                        "utc_datetime_added "
+                        "device_name dns ipv4 "
+                        "notifications_list "
+                        "recurring_downtime "
+                        "maintenance_downtime")
+    while True:
+
+        print("service running", time.gmtime())
+        time.sleep(60)
+
 
 
 def main():
-    options = [
-        "user_administration",
-        "add monitoring",
-        "manual server check",
-    ]
-    print(show_menu(options))
+
+    options = {
+        "user administration": user_administration,
+        "add monitoring": monitoring,
+        "manual server check": manual_server_check,
+        "send test email":send_email
+        "run service": run_service
+    }
+    selection = show_menu([each for each in options.keys()])
+    options[selection]()  # call the function from the dictionary
 
 
 if __name__ == '__main__':
